f783225ac1e1b55349d41e8d122d746cce2187a2
Yaroslav Stetsko <yar.stetsko@gmail.com>
2022-11-10 20:36:59 +0200

    Added login/logout functionality

--- restaurant_kitchen/urls.py
+++ restaurant_kitchen/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("kitchen.urls", namespace="kitchen")),
-
-]
+    path("accounts/", include("django.contrib.auth.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
