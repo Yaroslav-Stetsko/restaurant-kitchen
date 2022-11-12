abc8b5774cd5c31d07cc226fb0bc41150aa7ef16
Yaroslav Stetsko <yar.stetsko@gmail.com>
2022-11-12 16:44:34 +0200

    Minor redesign and ordering changes

--- kitchen/models.py
+++ kitchen/models.py
@@ -7,7 +7,7 @@ class DishType(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     class Meta:
-        ordering = ["name"]
+        ordering = ["id"]
 
     def __str__(self):
         return self.name
