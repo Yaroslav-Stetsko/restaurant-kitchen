96a9cb90002fd02ea44772ba14cf9ee6ba7d3760
Yaroslav Stetsko <yar.stetsko@gmail.com>
2022-11-15 12:38:15 +0200

    Added validation for years of experience

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -1,11 +1,15 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 from kitchen.models import Cook, Dish
 
 
 class CookCreationForm(UserCreationForm):
+    MIN_EXP = 0
+    MAX_EXP = 65
+
     class Meta(UserCreationForm.Meta):
         model = Cook
         fields = UserCreationForm.Meta.fields + (
@@ -14,8 +18,26 @@ class CookCreationForm(UserCreationForm):
             "last_name",
         )
 
+    def clean_years_of_experience(self):
+        years_of_experience = self.cleaned_data["years_of_experience"]
+
+        if years_of_experience > CookUpdateForm.MAX_EXP:
+            raise ValidationError(
+                f"Years of experience must be less than {CookUpdateForm.MAX_EXP}"
+            )
+
+        if years_of_experience < CookUpdateForm.MIN_EXP:
+            raise ValidationError(
+                f"Years of experience must be more than {CookUpdateForm.MIN_EXP}"
+            )
+
+        return years_of_experience
+
 
 class CookUpdateForm(forms.ModelForm):
+    MIN_EXP = 0
+    MAX_EXP = 65
+
     class Meta:
         model = Cook
         fields = UserCreationForm.Meta.fields + (
@@ -23,6 +45,21 @@ class CookUpdateForm(forms.ModelForm):
             "first_name",
             "last_name",
         )
+
+    def clean_years_of_experience(self):
+        years_of_experience = self.cleaned_data["years_of_experience"]
+
+        if years_of_experience > CookUpdateForm.MAX_EXP:
+            raise ValidationError(
+                f"Years of experience must be less than {CookUpdateForm.MAX_EXP}"
+            )
+
+        if years_of_experience < CookUpdateForm.MIN_EXP:
+            raise ValidationError(
+                f"Years of experience must be more than {CookUpdateForm.MIN_EXP}"
+            )
+
+        return years_of_experience
 
 
 class CookSearchForm(forms.Form):
