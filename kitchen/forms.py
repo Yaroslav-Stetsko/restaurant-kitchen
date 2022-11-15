b9b5f406c3f65ed9010bef606da52cc2214660c7
Yaroslav Stetsko <yar.stetsko@gmail.com>
2022-11-15 12:35:34 +0200

    Added validation for years of experience

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -1,21 +1,40 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 from kitchen.models import Cook, Dish
 
 
 class CookCreationForm(UserCreationForm):
+    MAX_EXP = 65
+    MIN_EXP = 0
+
     class Meta(UserCreationForm.Meta):
         model = Cook
         fields = UserCreationForm.Meta.fields + (
             "years_of_experience",
             "first_name",
             "last_name",
+
         )
+
+    def clean_years_of_experience(self):
+        years_of_experience = self.cleaned_data["years_of_experience"]
+
+        if years_of_experience > CookUpdateForm.MAX_EXP:
+            raise ValidationError(f"Years of experience must be less than {CookUpdateForm.MAX_EXP}")
+
+        if years_of_experience < CookUpdateForm.MIN_EXP:
+            raise ValidationError(f"Years of experience must be more than {CookUpdateForm.MIN_EXP}")
+
+        return years_of_experience
 
 
 class CookUpdateForm(forms.ModelForm):
+    MAX_EXP = 65
+    MIN_EXP = 0
+
     class Meta:
         model = Cook
         fields = UserCreationForm.Meta.fields + (
@@ -23,6 +42,17 @@ class CookUpdateForm(forms.ModelForm):
             "first_name",
             "last_name",
         )
+
+    def clean_years_of_experience(self):
+        years_of_experience = self.cleaned_data["years_of_experience"]
+
+        if years_of_experience > CookUpdateForm.MAX_EXP:
+            raise ValidationError(f"Years of experience must be less than {CookUpdateForm.MAX_EXP}")
+
+        if years_of_experience < CookUpdateForm.MIN_EXP:
+            raise ValidationError(f"Years of experience must be more than {CookUpdateForm.MIN_EXP}")
+
+        return years_of_experience
 
 
 class CookSearchForm(forms.Form):
